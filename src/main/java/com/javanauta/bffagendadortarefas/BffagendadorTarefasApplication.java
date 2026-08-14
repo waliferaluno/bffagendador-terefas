4457e108b2bdebb7af8bb0640353e821cfa5f17d
Walifer <waliferkainan2008@gmail.com>
2026-08-14 10:42:04 -0300

    adicionado esqueleto do projeto

---
+++ src/main/java/com/javanauta/bffagendadortarefas/BffagendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffagendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffagendadorTarefasApplication.class, args);
+	}
+
+}
