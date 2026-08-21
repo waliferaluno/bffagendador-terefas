e86048bb52152b89e6ca06f5df293a15d4395326
Walifer <waliferkainan2008@gmail.com>
2026-08-21 12:51:59 -0300

    adicionado cron

--- src/main/java/com/javanauta/bffagendadortarefas/infraestructure/client/EmailClient.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infraestructure/client/EmailClient.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 
 }
